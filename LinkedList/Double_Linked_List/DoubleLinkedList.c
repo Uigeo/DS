bfe29d2386ca10be9de41e96dc565dd051669569
Uigeo <mug4019@gmail.com>
2017-06-05 21:00:01 +0900

    Add some fuctions

--- LinkedList/Double_Linked_List/DoubleLinkedList.c
+++ LinkedList/Double_Linked_List/DoubleLinkedList.c
@@ -11,4 +11,63 @@ typedef struct _Node{
 
 typedef struct _DLinkedList{
     _Node* head;
+    _Node* tail;
+    int count;
 }D_LinkedList;
+
+
+D_LinkedList* createList(){
+  D_LinkedList* newList = (D_LinkedList*)malloc(sizeof(D_LinkedList));
+  newList->head = NULL;
+  newList->tail = NULL;
+  newList->count = 0;
+  return newList;
+}
+
+node* createNode(){
+  node* newNode = (node*)malloc(sizeof(node));
+  newNode->item =0;
+  newNode->llink = NULL;
+  newNode->rlink = NULL;
+  return newNode;
+}
+
+void destroyList(D_LinkedList* list){
+
+}
+
+void insertToHead(D_LinkedList* list, int item){
+
+}
+
+void insertToTail(D_LinkedList* list, int item){
+
+}
+
+node* deleteNode(D_LinkedList* list, node* n){
+
+}
+
+node* searchNode(D_LinkedList* list, int item){
+
+}
+
+node* searchNodeR(D_LinkedList* list, int item){
+
+}
+
+D_LinkedList* getReverseList(){
+
+}
+
+D_LinkedList* getCopyList(){
+
+}
+
+void printForward(D_LinkedList* list){
+
+}
+
+void printReverse(D_LinkedList* list){
+
+}
